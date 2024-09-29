93084cb298826da0b763824f3128ad98986f20fc
maulanamaky <ahmadmakyy14@gmail.com>
2024-09-29 22:14:16 +0700

    ada perubahan lagi

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -2,14 +2,31 @@ const http = require('http');
 
 const requestListener = (req, res) =>{
     res.setHeader('Content-Type', 'text/html');
-
     res.statusCode = 200;
-    res.end('<h1>test</h1>')
+    
+    const {method, url} = req;
+
+    if (url === '/') {
+        
+        if(method == GET) {
+            res.end('<h1>Ini halaman homepage</h1>')
+        } else {
+            res.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`)
+        }
+
+    } else if (url === '/about') {
+        
+    } else {
+        res.end('<h1>Halaman tidak ditemukan!</h1>')
+    }
+
 }
 
 const server = http.createServer(requestListener);
 
 const port = 3000;
-server.listen(port);
-console.log(`Server running at port ${port}`)
+const host = "localhost";
 
+server.listen(port, host, () => {
+    console.log(`Server running at ${host} port ${port}`)
+});
